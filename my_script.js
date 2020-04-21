9e5682345ec79377cd52a60d870c8f8006b32e58
bridau19 <bridau19@gmail.com>
2020-04-21 09:23:37 -0700

    maybe?

--- my_script.js
+++ my_script.js
@@ -2,23 +2,23 @@
 var chinaSection = document.getElementById('china');
 chinaSection.addEventListener("click", chinaPage);
 function chinaPage() {
-  window.location.href = "https://bridau19.github.io/travel/china.html";
+  window.location.href = "https://bridau19.github.io/familyTravel/china.html";
 }
 
 var peruSection = document.getElementById('peru');
 peruSection.addEventListener("click", peruPage);
 function peruPage() {
-  window.location.href = "https://bridau19.github.io/travel/peru.html";
+  window.location.href = "https://bridau19.github.io/familyTravel/peru.html";
 }
 
 var europeSection = document.getElementById('europe');
 europeSection.addEventListener("click", europePage);
 function europePage() {
-  window.location.href = "https://bridau19.github.io/travel/europe.html";
+  window.location.href = "https://bridau19.github.io/familyTravel/europe.html";
 }
 
 var koreaSection = document.getElementById('korea');
 koreaSection.addEventListener("click", koreaPage);
 function koreaPage() {
-  window.location.href = "https://bridau19.github.io/travel/korea.html";
+  window.location.href = "https://bridau19.github.io/familyTravel/korea.html";
 }
